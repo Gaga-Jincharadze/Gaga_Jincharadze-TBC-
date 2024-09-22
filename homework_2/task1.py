663575a08d63613a431ecea01b305e29fc1ea195
Gaga_Jincharadze <jincharadzegaga2003@gmail.com>
2024-09-22 23:17:32 +0400

    add homework_2

---
+++ homework_2/task1.py
@@ -0,0 +1,11 @@
+
+#Task 1
+
+V = str(input("Enter True or False: "))
+
+if V == "True":
+    print("Whoala")
+elif V == "False":
+    print("You Entered False.")
+else:
+    print("Enter 'True' or 'False'.")
---
+++ homework_2/task2.py
@@ -0,0 +1,9 @@
+# #Task 2
+
+a = float(input("Enter Your Number: "))
+b = float(input("Enter Your Number: "))
+
+if b % a == 0:
+    print("jeradia")
+else:
+    print("ar aris jeradi")
