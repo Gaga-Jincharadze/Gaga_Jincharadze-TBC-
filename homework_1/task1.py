2a9cbdcfb91c485e13c31ebe90fb08c8cfc1ce59
Gaga_Jincharadze <jincharadzegaga2003@gmail.com>
2024-09-22 23:09:07 +0400

    add homework_1

---
+++ homework_1/task1.py
@@ -0,0 +1,7 @@
+#Task 1:
+
+name = input("შეიყვანეთ სახელი: ")
+surname = input("შეიყვანეთ გვარი: ")
+Age = input("შეიყვანეთ ასაკი: ")
+
+print("გამარჯობა,",name, surname,", თქვენ ხართ",Age," წლის" )
---
+++ homework_1/task4.py
@@ -0,0 +1,6 @@
+#Task 4:
+
+C = float(input("C: "))
+
+F = float((C * (9 / 5)) + 32)
+print("F =", F)
