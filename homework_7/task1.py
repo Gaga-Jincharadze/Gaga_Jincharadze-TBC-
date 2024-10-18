4ef9b65028320ae54a2ebf7f19cfedb0f9e177e7
Gaga_Jincharadze <jincharadzegaga2003@gmail.com>
2024-10-18 18:07:42 +0400

    add homework_8

--- homework_7/task1.py
+++
@@ -1,5 +0,0 @@
-String = str(input("Enter String: "))
-
-for i in range(0,len(String)):
-        if i % 2 == 0 and String[i] != 'e':
-            print(String[i], end="")
--- homework_7/task2.py
+++
@@ -1,5 +0,0 @@
-String = str(input("Enter String: "))
-
-for i in range(0,len(String)):
-        if String[i] not in  {'e','a','o','i','u'}:
-            print(String[i], end="")
