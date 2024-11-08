c61abcaed8e1ec5fd470c9be96de9def7bd910e4
Gaga_Jincharadze <jincharadzegaga2003@gmail.com>
2024-11-08 15:24:02 +0400

    add homework_14

--- homework_13/task2.py
+++
@@ -1,9 +0,0 @@
-import random
-
-list1 = [random.randint(1, 1000) for _ in range(10)]
-list2 = [random.randint(1, 1000) for _ in range(10)]
-list3 = [random.randint(1, 1000) for _ in range(10)]
-
-result = list(map(lambda x, y, z: x + y + z , list1, list2, list3))
-
-print("Sum Of x&y&z:", result)
--- homework_13/task3.py
+++
@@ -1,5 +0,0 @@
-text = ["gaga","tako","gia","jora","ana","tbc"]
-
-for i in map(lambda x: x , text ):
-    if len(i) <= 3:
-        print(i.upper())
