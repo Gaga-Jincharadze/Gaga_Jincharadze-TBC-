f58bd91f45188c8bf9f36d42ecb2e64f86490956
Gaga_Jincharadze <jincharadzegaga2003@gmail.com>
2024-11-11 17:45:39 +0400

    add gaga

--- homework_14/task2.py
+++
@@ -1,9 +0,0 @@
-def midpoint(x1, y1, x2, y2):
-    a = (x1 + x2) / 2
-    b = (y1 + y2) / 2
-    return (a, b)
-
-print(midpoint(1, 4, 5, 8))
-print(midpoint(0, 0, 10, 10))
-print(midpoint(-3, 5, 7, -1))
-print(midpoint(2.5, 4.5, 5.5, 8.5))
